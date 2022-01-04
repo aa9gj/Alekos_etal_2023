d40e316daa6c7071e4d36d5de134252c0929911d
aa9gj <aa9gj@virginia.edu>
2022-01-04 17:43:18 -0500

    update heatmaps

--- riddle_2022_analysis.R
+++ riddle_2022_analysis.R
@@ -20,6 +20,8 @@ dds <- estimateSizeFactors(dds)
 # Extract the normalized counts
 normalized_counts <-counts(dds, normalize = TRUE)
 normalized_counts <- as.data.frame(normalized_counts)
+normalized_counts_mod <- setDT(normalized_counts, keep.rownames = T)[]
+normalized_counts_mod <- separate(normalized_counts_mod, rn, into = c("gene_id", "gene_name"), sep = "\\|")
 # Transform the normalized counts
 vsd <- vst(dds, blind = TRUE)
 #Extract the matrix of transformed counts
@@ -53,3 +55,47 @@ con_vs_treat_res <- as.data.frame(con_vs_treat_res)
 con_vs_treat_res <- setDT(con_vs_treat_res, keep.rownames = T)[]
 con_vs_treat_res <- separate(con_vs_treat_res, rn, into = c("gene_id", "gene_name"), sep = "\\|")
 write.csv(con_vs_treat_res, "/Users/aa9gj/Documents/Riddle_2022_analysis/diff_exp_sig_results", quote = F, row.names = F)
+
+##create heatmaps
+
+fatty_acid <- read.table("/Users/aa9gj/Documents/Riddle_2022_analysis/fatty_acid.txt", header = F)
+glycolysis <- read.table("/Users/aa9gj/Documents/Riddle_2022_analysis/glycolysis.txt", header = F)
+amino_acid <- read.table("/Users/aa9gj/Documents/Riddle_2022_analysis/amino_acid.txt", header = F)
+
+fattyacid_norm_counts <- inner_join(fatty_acid, normalized_counts_mod, by = c("V1"="gene_name"))
+glycolysis_norm_counts <- inner_join(glycolysis, normalized_counts_mod, by = c("V1"="gene_name"))
+aminoacid_norm_counts <- inner_join(amino_acid, normalized_counts_mod, by = c("V1"="gene_name"))
+
+# #emailed Ryan about these genes 
+# grep("Ascl2", normalized_counts_mod$gene_name) #might have to change genes in the list by ryan to these
+# grep("Pfkfb3", normalized_counts_mod$gene_name)# same
+# grep("Slc36a4", normalized_counts_mod$gene_name)#same
+# x <- grep("Gls", normalized_counts_mod$gene_name)
+
+##make them nicer and send to Ryan
+fattyacid_norm_counts <- fattyacid_norm_counts[,-2]
+row.names(fattyacid_norm_counts) <- fattyacid_norm_counts$V1
+fattyacid_norm_counts[1] <- NULL
+pheatmap(fattyacid_norm_counts)
+mat <- log2(fattyacid_norm_counts)
+is.na(mat)<-sapply(mat, is.infinite)
+mat[is.na(mat)]<-0
+pheatmap(mat)
+
+glycolysis_norm_counts <- glycolysis_norm_counts[,-2]
+row.names(glycolysis_norm_counts) <- glycolysis_norm_counts$V1
+glycolysis_norm_counts[1] <- NULL
+pheatmap(glycolysis_norm_counts)
+mat <- log2(glycolysis_norm_counts)
+is.na(mat)<-sapply(mat, is.infinite)
+mat[is.na(mat)]<-0
+pheatmap(mat)
+
+aminoacid_norm_counts <- aminoacid_norm_counts[,-2]
+row.names(aminoacid_norm_counts) <- aminoacid_norm_counts$V1
+aminoacid_norm_counts[1] <- NULL
+pheatmap(aminoacid_norm_counts)
+mat <- log2(aminoacid_norm_counts)
+is.na(mat)<-sapply(mat, is.infinite)
+mat[is.na(mat)]<-0
+pheatmap(mat)
